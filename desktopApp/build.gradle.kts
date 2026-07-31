76619110ad98bd00dc83854aef556e64e28032b3
maxx52 <gospelwork@yandex.ru>
2026-07-31 23:32:09 +0700

    feat(planner): move planner UI into platform

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "org.holypresenter"
-version = "0.5.0"
+version = "0.6.0"
 
 repositories {
     mavenLocal()
@@ -18,6 +18,7 @@ dependencies {
     implementation("org.jetbrains.compose.runtime:runtime:1.11.1")
     implementation("org.jetbrains.compose.foundation:foundation:1.11.1")
     implementation(libs.androidx.material3.desktop)
+    implementation("org.holypresenter:platform-api:0.5.0")
 }
 
 publishing {
